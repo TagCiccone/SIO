7fbea979d525462a78bd40ef155e8582b513e8f8
tagciccone <tagciccone@gmail.com>
2026-04-27 17:11:53 -0400

    replace getevent with regex

--- controllers.h
+++ controllers.h
@@ -2,6 +2,8 @@
 #include <map>
 
 #include "common.h"
+#include <regex>
+#include <fstream>
 
 namespace controller {
     enum INPUT_TYPE {
@@ -18,3 +20,34 @@ struct ControllerState {
 enum CONTROLLER_TYPE {
     XBOX,
 };
+
+std::string getEvent(CONTROLLER_TYPE controllerType) {
+    std::string idStr;
+    switch (controllerType) {
+        case XBOX: idStr = "Xbox"; break;
+    }
+    std::regex pattern(R"(event(\d+))");
+
+    std::ifstream f("/proc/bus/input/devices");
+
+    std::string line;
+    bool found_controller = false;
+    std::string event;
+    while (std::getline(f, line)) {
+        if (!found_controller && line.find(idStr) != std::string::npos) {
+            found_controller = true;
+        } else if (found_controller && line.find("Handlers") != std::string::npos) {
+            std::smatch matches;
+
+            if (std::regex_search(line, matches, pattern)) {
+                event = matches[1].str();
+                break;
+            }
+        }
+    } 
+
+    f.close();
+
+    return event;
+
+}
